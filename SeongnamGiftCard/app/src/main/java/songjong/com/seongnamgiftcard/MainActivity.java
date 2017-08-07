7e940162c85a287df0e435563a056efd64297750
ghdhddnr92 <ghdehddnr92@gmail.com>
2017-08-07 22:26:44 +0900

    세번째 커밋

--- SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/MainActivity.java
+++ SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/MainActivity.java
@@ -2,26 +2,65 @@ package songjong.com.seongnamgiftcard;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import songjong.com.seongnamgiftcard.TabFragment.TabPagerAdapter;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar 초기화 부분
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //TabLayout 초기화 부분
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab One"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Two"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Three"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //View Pager 초기화 부분
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        // TapPageAdapter 초기화 부분
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -99,4 +138,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
---
+++ SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/TabFragment/EtcTabFragment.java
@@ -0,0 +1,8 @@
+package songjong.com.seongnamgiftcard.TabFragment;
+
+/**
+ * Created by dongwook on 2017. 8. 7..
+ */
+
+public class EtcTabFragment {
+}
