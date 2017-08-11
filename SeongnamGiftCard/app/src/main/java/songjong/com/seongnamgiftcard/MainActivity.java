edbc456a8d9a6c0057acabf91e2d597a6d421ec8
Tae-Hyung Kim <xodaktmdnj@naver.com>
2017-08-11 16:17:46 +0900

    listView 최종완성(보류 -> recyclerView 구현할 예정)

--- SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/MainActivity.java
+++ SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/MainActivity.java
@@ -31,8 +31,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ViewPager viewPager;
     private FloatingActionMenu fam;
     private FloatingActionButton fabMap,fabCurrentPosition,fabSearch;
-    private ListView listView1;
-    private IconTextListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -116,39 +114,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        //listView 초기화 부분
-        listView1 = (ListView) findViewById(R.id.listView1);
-        adapter = new IconTextListAdapter(this);
-        // 아이템 데이터 만들기
-
-        Resources res = getResources();
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "추억의 테트리스", "30,000 다운로드", "900 원"));
-        /*
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "고스톱 - 강호동 버전", "26,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "친구찾기 (Friends Seeker)", "300,000 다운로드", "900 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "강좌 검색", "120,000 다운로드", "900 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - 서울", "4,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 도쿄", "6,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - LA", "8,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 워싱턴", "7,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - 파리", "9,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 베를린", "38,000 다운로드", "1500 원"));
-        //리스트뷰에 어댑터 설정
-        */
-        listView1.setAdapter(adapter);
-
-        //리스너로 설정
-        /*
-        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                IconTextItem curItem = (IconTextItem) adapter.getItem(position);
-                String[] curData = curItem.getData();
-
-                Toast.makeText(getApplicationContext(), "Selected : " + curData[0], Toast.LENGTH_LONG).show();
-
-            }
-        });*/
 
     }
 
--- SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/TabFragment/FoodTabFragment.java
+++ SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/TabFragment/FoodTabFragment.java
@@ -1,11 +1,15 @@
 package songjong.com.seongnamgiftcard.TabFragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import songjong.com.seongnamgiftcard.IconTextItem;
+import songjong.com.seongnamgiftcard.IconTextListAdapter;
 import songjong.com.seongnamgiftcard.R;
 
 /**
@@ -13,10 +17,48 @@ import songjong.com.seongnamgiftcard.R;
  */
 
 public class FoodTabFragment extends Fragment {
-
+    private ListView listView1;
+    private IconTextListAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_food, container, false);
+
+        View view = inflater.inflate(R.layout.tab_fragment_food, container, false);
+        //listView 초기화 부분
+
+        listView1 = (ListView) view.findViewById(R.id.listView1);
+        adapter = new IconTextListAdapter(getActivity());
+        // 아이템 데이터 만들기
+
+        Resources res = getResources();
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "추억의 테트리스", "30,000 다운로드", "900 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "고스톱 - 강호동 버전", "26,000 다운로드", "1500 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "친구찾기 (Friends Seeker)", "300,000 다운로드", "900 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "강좌 검색", "120,000 다운로드", "900 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - 서울", "4,000 다운로드", "1500 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 도쿄", "6,000 다운로드", "1500 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - LA", "8,000 다운로드", "1500 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 워싱턴", "7,000 다운로드", "1500 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - 파리", "9,000 다운로드", "1500 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 베를린", "38,000 다운로드", "1500 원"));
+        //리스트뷰에 어댑터 설정
+
+        listView1.setAdapter(adapter);
+
+        //리스너로 설정
+        /*
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                IconTextItem curItem = (IconTextItem) adapter.getItem(position);
+                String[] curData = curItem.getData();
+
+                Toast.makeText(getApplicationContext(), "Selected : " + curData[0], Toast.LENGTH_LONG).show();
+
+            }
+        });*/
+        return view;
     }
 }
+
+
 
