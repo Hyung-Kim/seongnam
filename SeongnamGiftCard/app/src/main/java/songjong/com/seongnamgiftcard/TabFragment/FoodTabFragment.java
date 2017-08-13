83edafa397e8d24839ebd45c5a877a5dfbe570c2
Tae-Hyung Kim <xodaktmdnj@naver.com>
2017-08-13 16:54:41 +0900

    RecyclerView + cardView로 업체 리스팅 출력완료

--- SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/TabFragment/FoodTabFragment.java
+++ SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/TabFragment/FoodTabFragment.java
@@ -1,62 +1,52 @@
 package songjong.com.seongnamgiftcard.TabFragment;
 
-import android.content.res.Resources;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-
-import songjong.com.seongnamgiftcard.IconTextItem;
-import songjong.com.seongnamgiftcard.IconTextListAdapter;
+import java.util.ArrayList;
+import java.util.List;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import songjong.com.seongnamgiftcard.R;
+import songjong.com.seongnamgiftcard.Radio;
+import songjong.com.seongnamgiftcard.RecyclerViewAdapter;
+import android.support.design.widget.CollapsingToolbarLayout;
 
 /**
  * Created by dongwook on 2017. 8. 7..
  */
 
 public class FoodTabFragment extends Fragment {
-    private ListView listView1;
-    private IconTextListAdapter adapter;
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    private RecyclerViewAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.tab_fragment_food, container, false);
-        //listView 초기화 부분
 
-        listView1 = (ListView) view.findViewById(R.id.listView1);
-        adapter = new IconTextListAdapter(getActivity());
-        // 아이템 데이터 만들기
+        ButterKnife.bind(getActivity());
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new RecyclerViewAdapter(getActivity().getApplicationContext());
+        recyclerView.setAdapter(adapter);
+        loadData();
 
-        Resources res = getResources();
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "추억의 테트리스", "30,000 다운로드", "900 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "고스톱 - 강호동 버전", "26,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "친구찾기 (Friends Seeker)", "300,000 다운로드", "900 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "강좌 검색", "120,000 다운로드", "900 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - 서울", "4,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 도쿄", "6,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - LA", "8,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 워싱턴", "7,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "지하철 노선도 - 파리", "9,000 다운로드", "1500 원"));
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "지하철 노선도 - 베를린", "38,000 다운로드", "1500 원"));
-        //리스트뷰에 어댑터 설정
-
-        listView1.setAdapter(adapter);
-
-        //리스너로 설정
-        /*
-        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                IconTextItem curItem = (IconTextItem) adapter.getItem(position);
-                String[] curData = curItem.getData();
-
-                Toast.makeText(getApplicationContext(), "Selected : " + curData[0], Toast.LENGTH_LONG).show();
-
-            }
-        });*/
         return view;
+    }
+    private void loadData(){
+        Radio radio = new Radio("Joy Radio FM", R.drawable.temp, "102.5");
+        List<Radio> radioList = new ArrayList<>();
+        for (int i = 0 ; i < 20 ; i++)
+            radioList.add(radio);
+        adapter.setRadioList(radioList);
     }
 }
 
