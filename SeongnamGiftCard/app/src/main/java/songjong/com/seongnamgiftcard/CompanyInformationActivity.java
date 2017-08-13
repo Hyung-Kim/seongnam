de31292a2706fbcca604bca094a63e003fcd946f
Tae-Hyung Kim <xodaktmdnj@naver.com>
2017-08-13 22:08:57 +0900

    recycler view + card view 추가(업체 리스팅)

---
+++ SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/CompanyInformationActivity.java
@@ -0,0 +1,13 @@
+package songjong.com.seongnamgiftcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompanyInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_information);
+    }
+}
--- SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/TabFragment/FoodTabFragment.java
+++ SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/TabFragment/FoodTabFragment.java
@@ -1,22 +1,51 @@
 package songjong.com.seongnamgiftcard.TabFragment;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import java.util.ArrayList;
+import java.util.List;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import songjong.com.seongnamgiftcard.R;
+import songjong.com.seongnamgiftcard.Radio;
+import songjong.com.seongnamgiftcard.RecyclerViewAdapter;
+import android.support.design.widget.CollapsingToolbarLayout;
 
 /**
  * Created by dongwook on 2017. 8. 7..
  */
 
 public class FoodTabFragment extends Fragment {
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
 
+    private RecyclerViewAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_food, container, false);
+
+        View view = inflater.inflate(R.layout.tab_fragment_food, container, false);
+
+        ButterKnife.bind(getActivity());
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new RecyclerViewAdapter(getActivity().getApplicationContext());
+        recyclerView.setAdapter(adapter);
+        loadData();
+
+        return view;
+    }
+    private void loadData(){
+        Radio radio = new Radio("Joy Radio FM", R.drawable.temp, "102.5");
+        List<Radio> radioList = new ArrayList<>();
+        for (int i = 0 ; i < 20 ; i++)
+            radioList.add(radio);
+        adapter.setRadioList(radioList);
     }
 }
-
