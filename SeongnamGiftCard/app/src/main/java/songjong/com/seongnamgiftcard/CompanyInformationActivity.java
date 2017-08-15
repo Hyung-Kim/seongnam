e2f42c39f71465df9035a817410e76f79b0a36c1
Tae-Hyung Kim <xodaktmdnj@naver.com>
2017-08-15 21:10:23 +0900

    modify error code(CompanyInformationActivity : Uppercase -> lowercase)

--- SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/CompanyInformationActivity.java
+++ SeongnamGiftCard/app/src/main/java/songjong/com/seongnamgiftcard/CompanyInformationActivity.java
@@ -3,7 +3,7 @@ package songjong.com.seongnamgiftcard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ComPanyInformationActivity extends AppCompatActivity {
+public class CompanyInformationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
